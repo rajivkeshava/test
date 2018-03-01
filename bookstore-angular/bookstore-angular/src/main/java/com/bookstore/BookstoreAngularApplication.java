6b8cd06510694ab1f1adc049e5cdd4da4d1f5de3
rajivkeshava <36817197+rajivkeshava@users.noreply.github.com>
2018-03-01 16:55:05 +0530

    changes

--- bookstore-angular/bookstore-angular/src/main/java/com/bookstore/BookstoreAngularApplication.java
+++ bookstore-angular/bookstore-angular/src/main/java/com/bookstore/BookstoreAngularApplication.java
@@ -3,18 +3,23 @@ package com.bookstore;
 import java.util.HashSet;
 import java.util.Set;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import com.bookstore.config.SecurityUtility;
 import com.bookstore.domain.security.Role;
-import com.bookstore.domain.security.User;
+import com.bookstore.domain.User;
 import com.bookstore.domain.security.UserRole;
+import com.bookstore.service.UserService;
 
 @SpringBootApplication
 public class BookstoreAngularApplication implements CommandLineRunner {
 
+
+	@Autowired
+	private UserService userService;
 	public static void main(String[] args) {
 		SpringApplication.run(BookstoreAngularApplication.class, args);
 	}
@@ -24,28 +29,28 @@ public class BookstoreAngularApplication implements CommandLineRunner {
 		User user1= new User();
 		user1.setFirstName("rajiv");
 		user1.setFirstName("keshava");
-		user1.setUserName("rajivkeshava");
+		user1.setUsername("rajivkeshava");
 		user1.setPassword(SecurityUtility.passwordEncoder().encode("abcd1234"));
-		user1.seteMail("rajiv.keUserRoles.com");
+		user1.setEmail("rajiv.keUserRoles.com");
 		Set<UserRole> userRoles = new HashSet<>();
 		Role role1 = new Role();
 		role1.setRoleId(1);
 		role1.setName("ROLE_USER");
-		userRoles.add(new UserRole(user1,role1));
+		userRoles.add(new UserRole(user1, role1));
+
 		userService.createUser(user1,userRoles);
 		userRoles.clear();
 
 		User user2= new User();
 		user2.setFirstName("admin");
 		user2.setFirstName("admin");
-		user2.setUserName("admin");
+		user2.setUsername("admin");
 		user2.setPassword(SecurityUtility.passwordEncoder().encode("abcd1234"));
-		user2.seteMail("rajiv.admin.com");
-		Set<UserRole> userRoles = new HashSet<>();
-		Role role1 = new Role();
+		user2.setEmail("rajiv.admin.com");
+		Role role2 = new Role();
 		role1.setRoleId(0);
 		role1.setName("ROLE_ADMIN");
-		userRoles.add(new UserRole(user2,role1));
+		userRoles.add(new UserRole(user2,role2));
 		userService.createUser(user2,userRoles);
 		userRoles.clear();
 
