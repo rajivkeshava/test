342bc5858dcd39ec48e0142cb9d7a53994a090a7
rajivkeshava <36817197+rajivkeshava@users.noreply.github.com>
2018-03-02 06:57:32 +0530

    changes

--- bookstore-angular/bookstore-angular/src/main/java/com/bookstore/repository/BookRepository.java
+++ bookstore-angular/bookstore-angular/src/main/java/com/bookstore/repository/BookRepository.java
@@ -1,23 +1,11 @@
-package com.bookstore.resource;
+package com.bookstore.repository;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import java.util.List;
+
+import org.springframework.data.repository.CrudRepository;
 
 import com.bookstore.domain.Book;
-import com.bookstore.service.BookService;
 
-@RestController
-@RequestMapping("/book")
-public class BookResource {
-	
-	@Autowired
-	private BookService bookService;
-	
-	@RequestMapping (value="/add", method=RequestMethod.POST)
-	public Book addBookPost(@RequestBody Book book) {
-		return bookService.save(book);
-	}
+public interface BookRepository extends CrudRepository<Book, Long>{
+	List<Book> findByTitleContaining(String keyword);
 }
